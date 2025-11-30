483471ddea8043ecb373f4ba3fd6186f8ecac3bc
Philippe Marschall <philippe.marschall@gmail.com>
2025-11-30 17:46:49 +0100

    read recording in tests

--- src/test/java/com/github/marschall/jnicriticalreporter/CompressionTests.java
+++ src/test/java/com/github/marschall/jnicriticalreporter/CompressionTests.java
@@ -5,17 +5,55 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jdk.jfr.Recording;
+import jdk.jfr.consumer.RecordingFile;
+
 class CompressionTests {
+
+  private static final String JNI_CRITICAL_EVENT = "com.github.marschall.jnicriticalreporter.Event";
+  private Recording recording;
+
+  @BeforeEach
+  void startRecording() {
+    this.recording = new Recording();
+    this.recording.enable(JNI_CRITICAL_EVENT);
+    long oneMb = 1L * 1024L * 1024L;
+    this.recording.setMaxSize(oneMb);
+    this.recording.start();
+  }
+
+  @AfterEach
+  void stopRecording() throws IOException {
+    var recordingPath = Path.of("target", "CompressionTests.jfr");
+    this.recording.dump(recordingPath);
+    this.recording.close();
+    AtomicInteger eventCount = new AtomicInteger(0);
+    try (var recordingFile = new RecordingFile(recordingPath)) {
+      while (recordingFile.hasMoreEvents()) {
+        var event = recordingFile.readEvent();
+        var eventType = event.getEventType();
+        if (eventType.getName().equals(JNI_CRITICAL_EVENT)) {
+          eventCount.incrementAndGet();
+        }
+      }
+    }
+    assertTrue(eventCount.get() > 0, "no events encountered");
+  }
 
   @Test
   void roundTrip() throws IOException {
